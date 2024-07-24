24153e7ec7f3c32f73531a8a82d471787fe9d02e
Paul Kim <khyunwoo1@gmail.com>
2024-07-23 18:55:31 -0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Contacts } from "./components/contacts";
+
+function App() {
+  return (
+    <>
+      <h1>this is a test</h1>
+      <Contacts />
+    </>
+  );
+}
+
+export default App;
